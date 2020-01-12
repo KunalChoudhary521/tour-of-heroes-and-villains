35282b76894109afd13a3181ac31f7907518fe70
KunalChoudhary521 <kunal.choudhary@mail.utoronto.ca>
2020-01-11 19:33:10 -0500

    e2e-tests: Fixed existing E2E protractor tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -55,15 +55,15 @@ describe('Tutorial part 6', () => {
       navElts: navElts,
 
       appDashboardHref: navElts.get(0),
-      appDashboard: element(by.css('app-root app-dashboard')),
-      topHeroes: element.all(by.css('app-root app-dashboard > div h4')),
+      appDashboard: element(by.css('app-root app-dashboard-ui')),
+      topHeroes: element.all(by.css('app-root app-dashboard-ui > div h4')),
 
       appHeroesHref: navElts.get(1),
       appHeroes: element(by.css('app-root app-heroes')),
       allHeroes: element.all(by.css('app-root app-heroes li')),
       selectedHeroSubview: element(by.css('app-root app-heroes > div:last-child')),
 
-      heroDetail: element(by.css('app-root app-hero-detail > div')),
+      heroDetail: element(by.css('app-root app-hero-detail-ui > div')),
 
       searchBox: element(by.css('#search-box')),
       searchResults: element.all(by.css('.search-result li'))
