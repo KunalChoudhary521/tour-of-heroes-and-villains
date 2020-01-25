dd491a9f39cfb062099ff94343a4d8404a7b6c7a
KunalChoudhary521 <kunal.choudhary@mail.utoronto.ca>
2020-01-25 09:51:39 -0500

    heroes: Get Heroes from the store

--- src/app/heroes/heroes.container.ts
+++ src/app/heroes/heroes.container.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { HeroService } from '../hero.service';
-import { Observable, Subject, merge } from 'rxjs';
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Hero } from '../hero';
 import { Router } from '@angular/router';
-import { scan } from 'rxjs/operators';
+import { select, Store } from '@ngrx/store';
+import { AppState } from '../store';
+import { heroesSelector } from '../store/heroes.selector';
 
 
 @Component({
@@ -13,48 +14,20 @@ import { scan } from 'rxjs/operators';
                             (addHero)="add($event)"
                             (deleteHero)="delete($event)"></app-heroes-ui>`
 })
-export class HeroesContainerComponent implements OnInit {
-  getHeroes = new Subject();
-  deleteHero = new Subject();
-  addHero = new Subject();
+export class HeroesContainerComponent {
 
-  heroes$: Observable<Hero[]> = merge(this.getHeroes.asObservable(), this.deleteHero.asObservable(), this.addHero.asObservable())
-    .pipe(scan((acc, curr: { action: string, value?: Hero | Hero[] }) => {
-      switch (curr.action) {
-        case 'GET':
-          // @ts-ignore
-          return [...acc, ...curr.value];
-        case 'ADD':
-          return [...acc, curr.value];
-        case 'DEL':
-          return acc.filter(h => h !== curr.value);
-        default:
-          return acc;
-      }
-    }, []));
+  heroes$: Observable<Hero[]> = this.store.pipe(select(heroesSelector));
 
-  constructor(private heroService: HeroService,
-              private router: Router) { }
+  constructor(private router: Router, private store: Store<AppState>) { }
 
   navigateToHero(heroId: number) {
     this.router.navigate([`/detail/${heroId}`]);
   }
 
-  ngOnInit(): void {
-    this.heroService.getHeroes()
-                    .subscribe(heroes => this.getHeroes.next({action: 'GET', value: heroes}));
-  }
-
   add(name: string): void {
-    name = name.trim();
-    if (!name) { return; }
-    this.heroService.addHero({ name } as Hero)
-                    .subscribe(hero => this.addHero.next({action: 'ADD', value: hero}));
   }
 
   delete(heroToDelete: Hero): void {
-    this.heroService.deleteHero(heroToDelete)
-                    .subscribe(() => this.deleteHero.next({action: 'DEL', value: heroToDelete}));
   }
 
 
