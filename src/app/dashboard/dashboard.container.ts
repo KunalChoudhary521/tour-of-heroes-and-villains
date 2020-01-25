a9559e970b9af3270eaad0cdf23d8b18c741061f
KunalChoudhary521 <kunal.choudhary@mail.utoronto.ca>
2020-01-25 09:51:36 -0500

    dashboard: Get Heroes from the store

--- src/app/dashboard/dashboard.container.ts
+++ src/app/dashboard/dashboard.container.ts
@@ -1,11 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Hero } from '../hero';
 import { Router } from '@angular/router';
-import { Store, select } from '@ngrx/store';
+import { select, Store } from '@ngrx/store';
 import { heroesSelector } from '../store/heroes.selector';
-import * as HeroesActions from '../store/heroes.actions';
 import { AppState } from '../store';
 
 @Component({
@@ -13,16 +12,12 @@ import { AppState } from '../store';
   template: `<app-dashboard-ui [heroes]="topHeroes$ | async"
                                (selectHero)="navigateToHero($event)"></app-dashboard-ui>`
 })
-export class DashboardContainerComponent implements OnInit {
+export class DashboardContainerComponent {
   topHeroes$: Observable<Hero[]> = this.store.pipe(select(heroesSelector), map(heroes => heroes.slice(1, 5)));
 
   constructor(private router: Router, private store: Store<AppState>) { }
 
   navigateToHero(heroId: number) {
     this.router.navigate([`/detail/${heroId}`]);
-  }
-
-  ngOnInit() {
-    this.store.dispatch(HeroesActions.getHeroes());
   }
 }
