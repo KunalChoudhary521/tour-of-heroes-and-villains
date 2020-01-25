7bbedc96e8119b9dd354376045e7a2b40adb51e3
KunalChoudhary521 <kunal.choudhary@mail.utoronto.ca>
2020-01-25 09:51:33 -0500

    app-root: Dispatch Get Heroes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from './store';
+import * as HeroesActions from './store/heroes.actions';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Tour of Heroes';
+
+  constructor(private store: Store<AppState>) { }
+
+  ngOnInit() {
+    // Dispatch here so that Dashboard or Heroes component get the heroes when application loads
+    this.store.dispatch(HeroesActions.getHeroes());
+  }
 }
