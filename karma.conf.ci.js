50bf3a70e93e3f276ac5741fb2fb9df162019c64
KunalChoudhary521 <kunal.choudhary@mail.utoronto.ca>
2022-11-26 19:28:52 -0500

    karma.conf: Run chrome headless with no-sandbox flag

--- karma.conf.ci.js
+++ karma.conf.ci.js
@@ -5,8 +5,15 @@ module.exports = function(config){
   baseConfig(config);
 
   // Override base config for CI build
+  // --no-sandbox flag: https://github.com/karma-runner/karma-chrome-launcher/issues/158#issuecomment-339265457
   config.set({
-    browsers: ['ChromeHeadless'],
+    browsers: ['ChromeHeadlessNoSandbox'],
+    customLaunchers:{
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     singleRun: true,
     autoWatch: false
   });
